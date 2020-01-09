9d59e1bccee6fa7d879f0bb6b16ad92ebabca695
DilipKumar-code <59668279+DilipKumar-code@users.noreply.github.com>
2020-01-09 17:02:24 +0530

    added indentation

--- src/main/java/de/androidpit/colorthief/ColorThief.java
+++ src/main/java/de/androidpit/colorthief/ColorThief.java
@@ -15,19 +15,14 @@
  * Lokesh Dhakar - for the original Color Thief JavaScript version
  * available at http://lokeshdhakar.com/projects/color-thief/
  */
-
 package de.androidpit.colorthief;
-
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.util.Arrays;
-
 import de.androidpit.colorthief.MMCQ.CMap;
-
 public class ColorThief {
-private static final int DEFAULT_QUALITY = 10;
-private static final boolean DEFAULT_IGNORE_WHITE = true;
-
+    private static final int DEFAULT_QUALITY = 10;
+    private static final boolean DEFAULT_IGNORE_WHITE = true;
     /**
      * Use the median cut algorithm to cluster similar colors and return the base color from the
      * largest cluster.
@@ -37,15 +32,14 @@ private static final boolean DEFAULT_IGNORE_WHITE = true;
      *
      * @return the dominant color as RGB array
      */
-public static int[] getColor(BufferedImage sourceImage) {
-int[][] palette = getPalette(sourceImage, 5);
-if (palette == null) {
-return null;
-}
-int[] dominantColor = palette[0];
-return dominantColor;
-}
-
+    public static int[] getColor(BufferedImage sourceImage) {
+        int[][] palette = getPalette(sourceImage, 5);
+        if (palette == null) {
+            return null;
+        }
+        int[] dominantColor = palette[0];
+        return dominantColor;
+    }
     /**
      * Use the median cut algorithm to cluster similar colors and return the base color from the
      * largest cluster.
@@ -63,15 +57,14 @@ return dominantColor;
      * @throws IllegalArgumentException
      *             if quality is &lt; 1
      */
-public static int[] getColor(BufferedImage sourceImage, int quality, boolean ignoreWhite) {
-int[][] palette = getPalette(sourceImage, 5, quality, ignoreWhite);
-if (palette == null) {
-return null;
-}
-int[] dominantColor = palette[0];
-return dominantColor;
-}
-
+    public static int[] getColor(BufferedImage sourceImage, int quality, boolean ignoreWhite) {
+        int[][] palette = getPalette(sourceImage, 5, quality, ignoreWhite);
+        if (palette == null) {
+            return null;
+        }
+        int[] dominantColor = palette[0];
+        return dominantColor;
+    }
     /**
      * Use the median cut algorithm to cluster similar colors.
      * 
@@ -82,14 +75,13 @@ return dominantColor;
      * 
      * @return the palette as array of RGB arrays
      */
-public static int[][] getPalette(BufferedImage sourceImage, int colorCount) {
-CMap cmap = getColorMap(sourceImage, colorCount);
-if (cmap == null) {
-return null;
-}
-return cmap.palette();
-}
-
+    public static int[][] getPalette(BufferedImage sourceImage, int colorCount) {
+        CMap cmap = getColorMap(sourceImage, colorCount);
+        if (cmap == null) {
+            return null;
+        }
+        return cmap.palette();
+    }
     /**
      * Use the median cut algorithm to cluster similar colors.
      * 
@@ -108,18 +100,17 @@ return cmap.palette();
      * @throws IllegalArgumentException
      *             if quality is &lt; 1
      */
-public static int[][] getPalette(
-BufferedImage sourceImage,
-int colorCount,
-int quality,
-boolean ignoreWhite) {
-CMap cmap = getColorMap(sourceImage, colorCount, quality, ignoreWhite);
-if (cmap == null) {
-return null;
-}
-return cmap.palette();
-}
-
+    public static int[][] getPalette(
+            BufferedImage sourceImage,
+            int colorCount,
+            int quality,
+            boolean ignoreWhite) {
+        CMap cmap = getColorMap(sourceImage, colorCount, quality, ignoreWhite);
+        if (cmap == null) {
+            return null;
+        }
+        return cmap.palette();
+    }
     /**
      * Use the median cut algorithm to cluster similar colors.
      * 
@@ -133,7 +124,6 @@ return cmap.palette();
     public static CMap getColorMap(BufferedImage sourceImage, int colorCount) {
         return getColorMap(sourceImage, colorCount, DEFAULT_QUALITY, DEFAULT_IGNORE_WHITE);
     }
-
     /**
      * Use the median cut algorithm to cluster similar colors.
      * 
@@ -163,24 +153,19 @@ return cmap.palette();
         if (quality < 1) {
             throw new IllegalArgumentException("Specified quality should be greater then 0.");
         }
-
         int[][] pixelArray;
-
         switch (sourceImage.getType()) {
         case BufferedImage.TYPE_3BYTE_BGR:
         case BufferedImage.TYPE_4BYTE_ABGR:
             pixelArray = getPixelsFast(sourceImage, quality, ignoreWhite);
             break;
-
         default:
             pixelArray = getPixelsSlow(sourceImage, quality, ignoreWhite);
         }
-
         // Send array to quantize function which clusters values using median cut algorithm
         CMap cmap = MMCQ.quantize(pixelArray, colorCount);
         return cmap;
     }
-
     /**
      * Gets the image's pixels via BufferedImage.getRaster().getDataBuffer(). Fast, but doesn't work
      * for all color models.
@@ -203,39 +188,31 @@ return cmap.palette();
         DataBufferByte imageData = (DataBufferByte) sourceImage.getRaster().getDataBuffer();
         byte[] pixels = imageData.getData();
         int pixelCount = sourceImage.getWidth() * sourceImage.getHeight();
-
         int colorDepth;
         int type = sourceImage.getType();
         switch (type) {
         case BufferedImage.TYPE_3BYTE_BGR:
             colorDepth = 3;
             break;
-
         case BufferedImage.TYPE_4BYTE_ABGR:
             colorDepth = 4;
             break;
-
         default:
             throw new IllegalArgumentException("Unhandled type: " + type);
         }
-
         int expectedDataLength = pixelCount * colorDepth;
         if (expectedDataLength != pixels.length) {
             throw new IllegalArgumentException(
                     "(expectedDataLength = " + expectedDataLength + ") != (pixels.length = "
                             + pixels.length + ")");
         }
-
         // Store the RGB values in an array format suitable for quantize function
-
         // numRegardedPixels must be rounded up to avoid an ArrayIndexOutOfBoundsException if all
         // pixels are good.
         int numRegardedPixels = (pixelCount + quality - 1) / quality;
-
         int numUsedPixels = 0;
         int[][] pixelArray = new int[numRegardedPixels][];
         int offset, r, g, b, a;
-
         // Do the switch outside of the loop, that's much faster
         switch (type) {
         case BufferedImage.TYPE_3BYTE_BGR:
@@ -244,7 +221,6 @@ return cmap.palette();
                 b = pixels[offset] & 0xFF;
                 g = pixels[offset + 1] & 0xFF;
                 r = pixels[offset + 2] & 0xFF;
-
                 // If pixel is not white
                 if (!(ignoreWhite && r > 250 && g > 250 && b > 250)) {
                     pixelArray[numUsedPixels] = new int[] {r, g, b};
@@ -252,7 +228,6 @@ return cmap.palette();
                 }
             }
             break;
-
         case BufferedImage.TYPE_4BYTE_ABGR:
             for (int i = 0; i < pixelCount; i += quality) {
                 offset = i * 4;
@@ -260,7 +235,6 @@ return cmap.palette();
                 b = pixels[offset + 1] & 0xFF;
                 g = pixels[offset + 2] & 0xFF;
                 r = pixels[offset + 3] & 0xFF;
-
                 // If pixel is mostly opaque and not white
                 if (a >= 125 && !(ignoreWhite && r > 250 && g > 250 && b > 250)) {
                     pixelArray[numUsedPixels] = new int[] {r, g, b};
@@ -268,15 +242,12 @@ return cmap.palette();
                 }
             }
             break;
-
         default:
             throw new IllegalArgumentException("Unhandled type: " + type);
         }
-
         // Remove unused pixels from the array
         return Arrays.copyOfRange(pixelArray, 0, numUsedPixels);
     }
-
     /**
      * Gets the image's pixels via BufferedImage.getRGB(..). Slow, but the fast method doesn't work
      * for all color models.
@@ -298,33 +269,25 @@ return cmap.palette();
             boolean ignoreWhite) {
         int width = sourceImage.getWidth();
         int height = sourceImage.getHeight();
-
         int pixelCount = width * height;
-
         // numRegardedPixels must be rounded up to avoid an ArrayIndexOutOfBoundsException if all
         // pixels are good.
         int numRegardedPixels = (pixelCount + quality - 1) / quality;
-
         int numUsedPixels = 0;
-
         int[][] res = new int[numRegardedPixels][];
         int r, g, b;
-
         for (int i = 0; i < pixelCount; i += quality) {
             int row = i / width;
             int col = i % width;
             int rgb = sourceImage.getRGB(col, row);
-
             r = (rgb >> 16) & 0xFF;
-            g = (rgb >> 8) & 0xFF;
+            g = (rgb >> 8& 0xFF;
             b = (rgb) & 0xFF;
             if (!(ignoreWhite && r > 250 && g > 250 && b > 250)) {
                 res[numUsedPixels] = new int[] {r, g, b};
                 numUsedPixels++;
             }
         }
-
         return Arrays.copyOfRange(res, 0, numUsedPixels);
     }
-
 }
